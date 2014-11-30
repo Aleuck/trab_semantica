908b4be876482b517fd8f32b8f096364ea640b8c
Alexandre Leuck <aleuck@inf.ufrgs.br>
2014-11-30 13:36:07 -0200

    operacional, exceções e listas

--- L1_ext2.ml
+++ L1_ext2.ml
@@ -79,7 +79,7 @@ let rec tipoDe e a = match e with
       let e = tipoDe e2 a 
         in if tipoDe e0 a = TBool
           then
-            if (tipoDe e1 a = e) 
+            if (tipoDe e1 a = e)
               then e 
               else raise TypeError
           else raise TypeError
@@ -117,13 +117,16 @@ let rec tipoDe e a = match e with
   | _ -> raise NoRuleApplies;;
 
 
+
+(* Operacional *)
+
 (* {v/x} t *)
 (* substitui : termo -> string -> termo -> termo *)
 let rec substitui v x t = match t with  
-    Op(op,t1,t2) -> Op(op, substitui v x t1, substitui v x t2)
-  | If(t1,t2,t3) -> If(substitui v x t1, substitui v x t2, substitui v x t3)
-  | App(t1,t2) -> App(substitui v x t1, substitui v x t2)
-  | Fun(y,tipo,termo) -> ( if compare x y == 0 
+    Op(op, t1, t2) -> Op(op, substitui v x t1, substitui v x t2)
+  | If(t1, t2, t3) -> If(substitui v x t1, substitui v x t2, substitui v x t3)
+  | App(t1, t2) -> App(substitui v x t1, substitui v x t2)
+  | Fun(y, tipo, termo) -> ( if compare x y == 0 
       then Fun(y, tipo, termo)
       else Fun(y, tipo, substitui v x termo))
   | Let(y, tipo, termo1, termo2) -> ( if compare x y == 0
@@ -135,16 +138,21 @@ let rec substitui v x t = match t with
   | Var(y) -> (if compare y x == 0
       then v
       else Var(y))
+  | Hd(t1) -> Hd(substitui v x t1)
+  | Tl(t1) -> Tl(substitui v x t1)
+  | Cons(t1, t2) -> Cons(substitui v x t1, substitui v x t2)
   |_-> t;;
 
 
-let rec passo t = match t with    
+let rec passo t = match t with
   (*Operações basicas*)
   (*op+*)
-   Op(Sub, Num e1, Num e2)-> Num (e1-e2)         
-  |Op(Soma, Num e1, Num e2)-> Num (e1+e2)         
+    Op(_,Raise,_) -> Raise
+  | Op(_,_,Raise) -> Raise
+  | Op(Sub, Num e1, Num e2)-> Num (e1-e2)
+  | Op(Soma, Num e1, Num e2)-> Num (e1+e2)
   (*op>=*)
-  | Op(MaiorIgual, Num e1, Num e2) -> if e1 >= e2 then Bool true else Bool false   
+  | Op(MaiorIgual, Num e1, Num e2) -> if e1 >= e2 then Bool true else Bool false
   (*OP2*)
   | Op(op, Num e1, e2)->
     let e3 = passo e2
@@ -156,6 +164,7 @@ let rec passo t = match t with
   (*IF*)
   | If(Bool true, e2, e3)->e2
   | If(Bool false, e2, e3)->e3
+  | If(Raise, _, _) -> Raise
   | If(t1, e2, e3)->
     let t1' = passo t1
         in If(t1', e2, e3)
@@ -164,18 +173,33 @@ let rec passo t = match t with
         Num _ -> (substitui t1 y t2)
       | Bool _ -> (substitui t1 y t2)
       | Fun(_,_,_) -> (substitui t1 y t2)
+      | Raise -> Raise
       | _ -> Let(y, tipo1, passo t1, t2))
   (* LET RET *)
   | LetR(f,t,e1,e2) -> (match e1 with
         Fun(x,tArg,efun) -> substitui (Fun(x, tArg, LetR(f,t,e1,efun))) f e2
-      | _ -> raise NoRuleApplies)
+      | Raise -> Raise
+      | _ -> LetR(f,t,(passo e1),e2))
   (*APP*)
+  | App(Raise,_) -> Raise
+  | App(_,Raise) -> Raise
   | App(Fun(y,tipo,t1), t2) -> (match t2 with
       Num _ -> substitui t2 y t1
     | Bool _ -> substitui t2 y t1
     | Fun(_,_,_) -> substitui t2 y t1
     | _ -> App(Fun(y,tipo,t1), passo t2))
   | App(t1,t2) -> App(passo t1, t2)
+  | Hd(Raise) -> Raise
+  | Hd(Cons(h,t)) -> h
+  | Tl(Raise) -> Raise
+  | Tl(Cons(h,t)) -> t
+  | Try(e1,e2) -> (match e1 with
+      Raise -> e2
+    | Num _ -> e1
+    | Bool _ -> e1
+    | Fun(_,_,_) -> e1
+    | Cons(_,_,_) -> e1
+    | _ -> Try(passo e1, e2))
   | _ -> raise NoRuleApplies;;
 
 
@@ -203,21 +227,23 @@ tipoDe (Cons(Num 0, Nil)) ambienteVazio ;;
 tipoDe (
 	LetR ( "y" ,  
 	TFun(TInt, TInt),   
-	Fun("x",  TInt, If(Op( MaiorIgual ,Num 0, Var "x" ), Num 0 , Op(Soma, Var "x", App(Var "y", Op(Sub, Var "x", Num 1) ))) ),          
+	Fun("x",  TInt, If(Op( MaiorIgual ,Num 0, Var "x" ), Num 0 , Op(Soma, Var "x", App(Var "y", Op(Sub, Var "x", Num 1) ))) ),
 	App(Var  "y", Num 5))
 		) []
 
 passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(passo(
 LetR ( "y" ,  
 	TFun(TInt, TInt),   
-	Fun("x", TInt , If(Op( MaiorIgual ,Num 0, Var "x" ), Num 0 , Op(Soma, Var "x", App(Var "y", Op(Sub, Var "x", Num 1) ))) ),          
+	Fun("x", TInt , If(Op( MaiorIgual ,Num 0, Var "x" ), Num 0 , Op(Soma, Var "x", App(Var "y", Op(Sub, Var "x", Num 1) ))) ),
 	App(Var  "y", Num 5))
 )))))))))))))))))))))))))))))))))))
 
 *)
 
 let rec run t = match t with
-| Num _ -> t
+  Num _ -> t
 | Bool _ -> t
 | Fun(_,_,_) -> t
+| Cons(_,_) -> t
+| Raise -> t
 | _ -> run (passo t) ;;
